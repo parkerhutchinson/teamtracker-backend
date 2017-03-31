cbf0c106e91e632affe26866558a354a3f7d6b48
Parker <parker@purplerockscissors.com>
2017-03-31 12:54:14 -0700

    removing unused imports

--- timetracking/views.py
+++ timetracking/views.py
@@ -1,12 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
 from timetracking.models import *
 from timetracking.serializers import TimeTrackingSerializer
-from django.http import Http404, HttpResponse
-from django.views import View
+from django.http import Http404
 
 
 class TimeTrackingAPIView(APIView):
