15a815f59b7ca916fff9dff2a55fc45ffedd4663
Parker <parker@purplerockscissors.com>
2017-04-09 19:03:44 -0700

    moved app urls into main.urls so there is a single point of entry for the apps. got a index.html file in for people navigating to the api url for whatever reason.

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url, include
 from main.views import *
 
 urlpatterns = [
-  url(r'^', IndexView.as_view())
+    url(r'^', include('profiles.urls')),
+    url(r'^', include('timetracking.urls')),
+    url(r'^', include('resources.urls')),
+    url(r'^', IndexView.as_view()),
 ]
